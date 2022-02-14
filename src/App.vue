b3342cc2af7766b09b3bf326c1c43905e91e42d2
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-14 18:28:39 +0100

    import axios, API

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div id="app">
 
+
   </div>
 </template>
 
 <script>
+  import axios from "axios";
  
 export default {
   name: 'App',
-  components: {
-    
-  }
+  data(){
+    return{
+      movies:[],
+      apiKey: '52506c224db8dc42f817a52dcdd3da51',
+      query: '',
+    }
+  },
+
+  methods:{
+    fetchMovies(){
+      const config = {
+        params:
+        {
+          apiKey: this.apiKey,
+          query: this.query,
+          lenguage: "it_IT",
+        }
+      };
+
+      axios.get(`https://api.themoviedb.org/3/search/movie`, config).then((res)=> {
+        this.movies = res.data.results;
+
+      })
+     }
+   }
+ 
 }
 </script>
 
 <style lang="scss">
- 
 </style>
