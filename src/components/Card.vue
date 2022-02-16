6eb5494d5abb11b2d4c50c8feec092d69eadb1a6
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-16 12:57:02 +0100

    added heigth  card, delete img-fluid

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="card-item bg-dark shadow m-1">
+  <div class="card-item shadow m-1">
     <img
-      class="img-cover img-fluid"
+      class="img-cover"
       v-if="item.poster_path"
       :src="`https://image.tmdb.org/t/p/w342/${item.poster_path}`"
       :alt="item.title || item.name"
     />
     <img
-      class="img-cover img-fluid"
+      class="img-cover"
       v-else
       src="https://www.altavod.com/assets/images/poster-placeholder.png"
       :alt="item.title || item.name"
     />
-    <div class="text-card">
-      <div>{{ item.title || item.name }}</div>
-      <div>{{ item.original_title || item.name }}</div>
-      <div>
+    <div class="text-card p-3">
+      <div class="mb-1">TITOLO: <span class="ms-1">{{ item.title || item.name }}</span></div>
+      <div class="mb-1">TITOLO originale: {{ item.original_title || item.name }}</div>
+      <div class="mb-1">LINGUA: 
         <img
           class="w-35"
           v-if="hasFlags"
@@ -26,12 +26,15 @@
           {{ item.original_language }}
         </span>
       </div>
-      <i
+      <div>VOTO: 
+         <i 
         v-for="n in 5"
         :key="n"
-        class="fa-star"
+        class="fa-star mb-1"
         :class="n <= voteItem ? 'fa-solid' : 'fa-regular'"
       ></i>
+      </div>
+     
     </div>
   </div>
 </template>
@@ -63,8 +66,8 @@ export default {
 <style lang="scss" >
 .card-item {
   position: relative;
-  width: 300px;
-  cursor: pointer;
+  background: black;
+  height: 500px;
 }
 .w-35 {
   width: 35px;
@@ -75,20 +78,22 @@ img {
 }
 
 .text-card {
+
   position: absolute;
-  top: 200px;
+  top: 0;
   left: 0;
   right: 0;
   bottom: 0;
-  text-align: center;
+  text-align: start;
   display: none;
 }
 
 .card-item:hover > .text-card {
+  opacity: 1;
   display: block;
 }
 
 .card-item:hover .img-cover{
-  display: none;
+  opacity: 0.1;
 }
 </style>
